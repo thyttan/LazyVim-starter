93fe81980f2bcf0a8492da3567c35389c5801958
thyttan <6uuxstm66@mozmail.com>
2023-03-02 17:15:59 +0100

    my personalizations

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,34 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- smart movement of selected lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- center cursor on different actions
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- paste over but don't update clipboard
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- quickfix list
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- when <esc> while visual mode selected code block go back to where the cursor was before
+local opts = { noremap = true }
+vim.keymap.set("n", "v", "m`v", opts)
+vim.keymap.set("n", "V", "m`V", opts)
+vim.keymap.set("n", "<C-V>", "m`<C-v>", opts)
+vim.keymap.set("x", "<Esc>", "<Esc>:keepjumps normal ``<cr>", opts)
+
+-- Remap for dealing with word wrap
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,42 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- linenumbering
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.numberwidth = 6
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "magenta" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "orange" })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "magenta" })
+
+-- remember undos between sessions
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+-- tab behavior
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = true
+
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Decrease update time
+--vim.o.updatetime = 250 -- already set above
+vim.wo.signcolumn = "yes"
